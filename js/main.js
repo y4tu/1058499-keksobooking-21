14a2ffc8e3ceb435a5d7911ca2967fa2e8461db5
y4tu <y4tu@yandex.ru>
2020-10-05 00:54:08 +0300

    correction according to video

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 
 const ADS_NUMBER = 8;
 const TITLES = [`Шейх`, `Лепс`, `Спарк`, `Ваниш`, `Афродита`, `Дженифер`, `Церцея`, `Бусилия`];
-const MIN_LOCATIONS_X = 100;
-const MAX_LOCATIONS_X = 1000;
+const MIN_LOCATIONS_X = 0;
+const MAX_LOCATIONS_X = 1200;
 const MIN_LOCATIONS_Y = 130;
 const MAX_LOCATIONS_Y = 630;
 const TYPES = [`palace`, `flat`, `house`, `bungalow`];
@@ -23,47 +23,39 @@ const PHOTOS = [
   `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
   `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
 ];
+const MULTIPLIER = 10000;
+const PIN_OFFSET_X = -25;
+const PIN_OFFSET_Y = -70;
+
 const map = document.querySelector(`.map`);
 const similarPins = map.querySelector(`.map__pins`);
 const mapPinTemplate = document.querySelector(`#pin`)
   .content
   .querySelector(`.map__pin`);
 
-
-const locationX = (min, max) => {
+const getRandomX = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const locationY = (min, max) => {
+const getRandomY = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const getTitle = () => TITLES[Math.floor(Math.random() * TITLES.length)];
-const getLocationX = () => locationX(MIN_LOCATIONS_X, MAX_LOCATIONS_X);
-const getLocationY = () => locationY(MIN_LOCATIONS_Y, MAX_LOCATIONS_Y);
-const getPrice = () => Math.round(Math.random() * 10000);
-const getType = () => TYPES[Math.floor(Math.random() * TYPES.length)];
-const getRoom = () => ROOMS[Math.floor(Math.random() * ROOMS.length)];
-const getGuest = () => GUESTS[Math.floor(Math.random() * GUESTS.length)];
-const getCheckin = () => CHECKIN[Math.floor(Math.random() * CHECKIN.length)];
-const getCheckout = () => CHECKOUT[Math.floor(Math.random() * CHECKOUT.length)];
-
-const getFeatures = () => {
-  const currentFeatures = [];
-
-  for (let i = 0; i < FEATURES.length - 3; i++) {
-    currentFeatures[i] = FEATURES[Math.floor(Math.random() * FEATURES.length)];
-  }
-
-  return currentFeatures;
+const getRandomArray = (array) => {
+  return array.slice(0, Math.floor(Math.random()));
 };
 
-const getDescription = () => DESCRIPTION[Math.floor(Math.random() * DESCRIPTION.length)];
-const getPhotos = () => PHOTOS[Math.floor(Math.random() * PHOTOS.length)];
+const getRandomArrayElement = (name) => name[Math.floor(Math.random() * name.length)];
+const getLocationX = () => getRandomX(MIN_LOCATIONS_X, MAX_LOCATIONS_X);
+const getLocationY = () => getRandomY(MIN_LOCATIONS_Y, MAX_LOCATIONS_Y);
+const getPrice = () => Math.round(Math.random() * MULTIPLIER);
+const getDescription = () => getRandomArrayElement(DESCRIPTION);
+const getFeature = () => getRandomArray(FEATURES);
+const getPhoto = () => getRandomArray(PHOTOS);
 
 const generateAds = (number) => {
   const ads = [];
@@ -71,20 +63,20 @@ const generateAds = (number) => {
   for (let i = 0; i < number; i++) {
     ads.push({
       author: {
-        avatar: `img/avatars/user0` + (i + 1).toString() + `.png`
+        avatar: `img/avatars/user0${(i + 1)}.png`
       },
-      offers: {
-        title: getTitle(),
+      offer: {
+        title: getRandomArrayElement(TITLES),
         address: `${getLocationX()}` + `, ${getLocationY()}`,
         price: getPrice(),
-        type: getType(),
-        rooms: getRoom(),
-        guests: getGuest(),
-        checkin: getCheckin(),
-        checkout: getCheckout(),
-        features: getFeatures(),
+        type: getRandomArrayElement(TYPES),
+        rooms: getRandomArrayElement(ROOMS),
+        guests: getRandomArrayElement(GUESTS),
+        checkin: getRandomArrayElement(CHECKIN),
+        checkout: getRandomArrayElement(CHECKOUT),
+        features: getFeature(),
         description: getDescription(),
-        photos: getPhotos(),
+        photos: getPhoto(),
       },
       location: {
         x: getLocationX(),
@@ -103,8 +95,8 @@ const createPin = (ad) => {
   const image = pinElement.querySelector(`img`);
 
   image.src = ad.author.avatar;
-  image.alt = ad.offers.title;
-  pinElement.style = `left: ${getLocationX() + 20}px; top: ${getLocationY() + 40}px;`;
+  image.alt = ad.offer.title;
+  pinElement.style = `left: ${getLocationX() + PIN_OFFSET_X}px; top: ${getLocationY() + PIN_OFFSET_Y}px;`;
 
   return pinElement;
 };
@@ -112,35 +104,10 @@ const createPin = (ad) => {
 const renderPins = (array) => {
   const fragment = document.createDocumentFragment();
 
-  for (let i = 0; i < array.length; i++) {
-    fragment.appendChild(createPin(array[i]));
-  }
+  array.forEach((item) => fragment.appendChild(createPin(item)));
 
   similarPins.appendChild(fragment);
 };
 
 const ads = generateAds(ADS_NUMBER);
 renderPins(ads);
-
-/*  {
-    "author": {
-    "avatar": строка, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
-  },
-    "offer": {
-        "title": строка, заголовок предложения
-        "address": строка, адрес предложения. Для простоты пусть пока представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
-        "price": число, стоимость
-        "type": строка с одним из четырёх фиксированных значений: palace, flat, house или bungalow
-        "rooms": число, количество комнат
-        "guests": число, количество гостей, которое можно разместить
-        "checkin": строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
-          "checkout": строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
-        "features": массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
-          "description": строка с описанием,
-          "photos": массив строк случайной длины, содержащий адреса фотографий "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-    },
-    "location": {
-    "x": случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-    "y": случайное число, координата y метки на карте от 130 до 630.
-  }
-  }*/
