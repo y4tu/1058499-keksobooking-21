aba0d8da729c0043e1fb8eb2c21a85a99ffbc549
y4tu <y4tu@yandex.ru>
2020-10-06 11:25:31 +0300

    fixes by git-comments

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 
 const ADS_NUMBER = 8;
 const TITLES = [`Шейх`, `Лепс`, `Спарк`, `Ваниш`, `Афродита`, `Дженифер`, `Церцея`, `Бусилия`];
-const MIN_LOCATIONS_X = 0;
-const MAX_LOCATIONS_X = 1200;
-const MIN_LOCATIONS_Y = 130;
-const MAX_LOCATIONS_Y = 630;
+const MIN_LOCATION_X = 0;
+const MAX_LOCATION_X = 1200;
+const MIN_LOCATION_Y = 130;
+const MAX_LOCATION_Y = 630;
 const TYPES = [`palace`, `flat`, `house`, `bungalow`];
 const ROOMS = [`1`, `2`, `3`, `4`];
 const GUESTS = [`2`, `4`, `6`, `8`];
@@ -23,9 +23,10 @@ const PHOTOS = [
   `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
   `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
 ];
-const MULTIPLIER = 10000;
-const PIN_OFFSET_X = -25;
-const PIN_OFFSET_Y = -70;
+const RANDOM_ARRAY_MULTIPLIER = 10;
+const PRICE_MULTIPLIER = 10000;
+const PIN_OFFSET_X = 25;
+const PIN_OFFSET_Y = 70;
 
 const map = document.querySelector(`.map`);
 const similarPins = map.querySelector(`.map__pins`);
@@ -33,26 +34,20 @@ const mapPinTemplate = document.querySelector(`#pin`)
   .content
   .querySelector(`.map__pin`);
 
-const getRandomX = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-const getRandomY = (min, max) => {
+const getRandomInRange = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 const getRandomArray = (array) => {
-  return array.slice(0, Math.floor(Math.random()));
+  return array.slice(0, Math.floor(Math.random() * array.length * RANDOM_ARRAY_MULTIPLIER));
 };
 
-const getRandomArrayElement = (name) => name[Math.floor(Math.random() * name.length)];
-const getLocationX = () => getRandomX(MIN_LOCATIONS_X, MAX_LOCATIONS_X);
-const getLocationY = () => getRandomY(MIN_LOCATIONS_Y, MAX_LOCATIONS_Y);
-const getPrice = () => Math.round(Math.random() * MULTIPLIER);
+const getRandomArrayElement = (array) => array[Math.floor(Math.random() * array.length)];
+const getLocationX = () => getRandomInRange(MIN_LOCATION_X, MAX_LOCATION_X);
+const getLocationY = () => getRandomInRange(MIN_LOCATION_Y, MAX_LOCATION_Y);
+const getPrice = () => Math.round(Math.random() * PRICE_MULTIPLIER);
 const getDescription = () => getRandomArrayElement(DESCRIPTION);
 const getFeature = () => getRandomArray(FEATURES);
 const getPhoto = () => getRandomArray(PHOTOS);
@@ -96,7 +91,7 @@ const createPin = (ad) => {
 
   image.src = ad.author.avatar;
   image.alt = ad.offer.title;
-  pinElement.style = `left: ${getLocationX() + PIN_OFFSET_X}px; top: ${getLocationY() + PIN_OFFSET_Y}px;`;
+  pinElement.style = `left: ${getLocationX() - PIN_OFFSET_X}px; top: ${getLocationY() - PIN_OFFSET_Y}px;`;
 
   return pinElement;
 };
