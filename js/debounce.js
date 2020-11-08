ed51d21b3bf10d68686691558918a902952b6311
y4tu <y4tu@yandex.ru>
2020-11-08 22:15:26 +0300

    iife removed from debounce

--- js/debounce.js
+++ js/debounce.js
@@ -1,22 +1,20 @@
 'use strict';
 
-(() => {
-  const DEBOUNCE_INTERVAL = 300;
+const DEBOUNCE_INTERVAL = 300;
 
-  const execute = (cb) => {
-    let lastTimeout = null;
+const execute = (cb) => {
+  let lastTimeout = null;
 
-    return (parameters) => {
-      if (lastTimeout) {
-        window.clearTimeout(lastTimeout);
-      }
-      lastTimeout = window.setTimeout(() => {
-        cb(parameters);
-      }, DEBOUNCE_INTERVAL);
-    };
+  return (parameters) => {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(() => {
+      cb(parameters);
+    }, DEBOUNCE_INTERVAL);
   };
+};
 
-  window.debounce = {
-    execute,
-  };
-})();
+window.debounce = {
+  execute,
+};
